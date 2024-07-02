bcf30d3fb22780faa850801e0cb7fc6860010ee5
SpieringsAE <maud_spierings@hotmail.com>
2024-07-02 13:36:24 +0200

    V1.0.1 changed to tokio+axum

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 to build run 
 ```
-RUSTFLAGS="-Zlocation-detail=none" cargo +nightly zigbuild -Z build-std=std,panic_abort --target aarch64-unknown-linux-gnu.2.31 --release
+RUSTFLAGS="-Zlocation-detail=none -C target-cpu=cortex-a53" cargo +nightly zigbuild -Z build-std=std,panic_abort --target aarch64-unknown-linux-gnu.2.31 --release
 ```
 and then
 ```
--- src/main.rs
+++ src/main.rs
@@ -1,232 +1,224 @@
-use std::process::{exit, Command};
+use std::process::Command;
 
-extern crate multipart;
-extern crate tiny_http;
+use axum::{extract::Multipart, http::StatusCode, response::Response, routing::post, Router};
 
-use multipart::server::{Multipart, SaveResult};
-use std::io::{self, Cursor};
-use tiny_http::{Request, Response, StatusCode};
-fn main() {
-    // Starting a server on `localhost:80`
-    let server = tiny_http::Server::http("0.0.0.0:8001").unwrap_or_else(|err| {
-        eprintln!("Could not bind 0.0.0.0:8001\n{}", err);
-        exit(-1);
-    });
-    loop {
-        // This blocks until the next request is received
-        let mut request = server.recv().unwrap();
+use tokio::net::TcpListener;
 
-        // Processes a request and returns response or an occured error
-        let result = process_request(&mut request);
-        let resp = match result {
-            Ok(resp) => resp,
-            Err(e) => {
-                println!("An error has occured during request proccessing: {:?}", e);
-                build_response(
-                    500,
-                    "The received data was not correctly proccessed on the server",
-                )
-            }
-        };
+const VERSION: &str = env!("CARGO_PKG_VERSION");
 
-        // Answers with a response to a client
-        _ = request.respond(resp);
-    }
+#[tokio::main(flavor = "current_thread")]
+async fn main() {
+    println!("GOcontroll upload server V{}", VERSION);
+    let app = Router::new().route("/upload", post(handle_upload));
+    let listener = TcpListener::bind("0.0.0.0:8001").await.unwrap();
+    axum::serve(listener, app.into_make_service())
+        .await
+        .unwrap();
 }
 
-type RespBody = Cursor<Vec<u8>>;
+async fn handle_upload(mut multipart: Multipart) -> Response<String> {
+    while let Some(field) = multipart.next_field().await.unwrap() {
+        match field.name().unwrap() {
+            "elfFile" => {
+                let Some(file_name) = field.file_name() else {
+                    return build_response(
+                        StatusCode::BAD_REQUEST,
+                        "No filename given, can't process\n".to_owned(),
+                    );
+                };
+                let Some(file_extension) = file_name.split(".").last() else {
+                    return build_response(
+                        StatusCode::BAD_REQUEST,
+                        "Could not get file extension, can't process\n".to_owned(),
+                    );
+                };
+                if file_extension != "elf" {
+                    return build_response(
+                        StatusCode::BAD_REQUEST,
+                        "Wrong file extension detected! Check your file!".to_owned(),
+                    );
+                }
 
-/// Processes a request and returns response or an occured error.
-fn process_request(request: &mut Request) -> io::Result<Response<RespBody>> {
-    // Getting a multipart reader wrapper
-    match Multipart::from_request(request) {
-        Ok(mut multipart) => {
-            while let Ok(Some(mut field)) = multipart.read_entry() {
-                match field.headers.name.as_ref() {
-                    "elfFile" => {
-                        let file_name = field.headers.filename.unwrap();
-                        let file_extension = file_name.split(".").last().unwrap();
-                        if file_extension != "elf" {
-                            return Ok(build_response(
-                                400,
-                                "Wrong file extension detected! Check your file!",
-                            ));
+                let restart = if Command::new("systemctl")
+                    .arg("is-active")
+                    .arg("--quiet")
+                    .arg("go-simulink")
+                    .status()
+                    .unwrap()
+                    .success()
+                {
+                    _ = Command::new("systemctl")
+                        .arg("stop")
+                        .arg("go-simulink")
+                        .status();
+                    true
+                } else {
+                    false
+                };
+
+                _ = std::fs::remove_file("/usr/simulink/GOcontroll_Linux.elf");
+
+                if !std::path::Path::new("/usr/simulink")
+                    .try_exists()
+                    .expect("can't check the existence of /usr/simulink")
+                {
+                    std::fs::create_dir("/usr/simulink").expect("could not create /usr/simulink");
+                }
+
+                let bytes = match field.bytes().await {
+                    Ok(bytes) => bytes,
+                    Err(err) => {
+                        return build_response(StatusCode::INTERNAL_SERVER_ERROR, err.to_string())
+                    }
+                };
+
+                match std::fs::write("/usr/simulink/GOcontroll_Linux.elf", bytes) {
+                    Ok(_) => {
+                        _ = Command::new("chmod")
+                            .args(["555", "/usr/simulink/GOcontroll_Linux.elf"])
+                            .status();
+                        if restart {
+                            _ = Command::new("systemctl")
+                                .arg("start")
+                                .arg("go-simulink")
+                                .spawn();
                         }
+                        _ = Command::new("sync").spawn();
+                        return build_response(StatusCode::OK, "elfFile uploaded!".to_owned());
+                    }
+                    Err(err) => {
+                        return build_response(
+                            StatusCode::INTERNAL_SERVER_ERROR,
+                            format!("Failed to write file to server.\n{}", err.to_string()),
+                        );
+                    }
+                }
+            }
+            "a2lFile" => {
+                let Some(file_name) = field.file_name() else {
+                    return build_response(
+                        StatusCode::BAD_REQUEST,
+                        "No filename given, can't process\n".to_owned(),
+                    );
+                };
+                let Some(file_extension) = file_name.split(".").last() else {
+                    return build_response(
+                        StatusCode::BAD_REQUEST,
+                        "Could not get file extension, can't process\n".to_owned(),
+                    );
+                };
+                if file_extension != "a2l" {
+                    return build_response(
+                        StatusCode::BAD_REQUEST,
+                        "Wrong file extension detected! Check your file!".to_owned(),
+                    );
+                }
 
-                        let restart = if Command::new("systemctl")
+                _ = std::fs::remove_file("/usr/simulink/GOcontroll_Linux.a2l");
+
+                let bytes = match field.bytes().await {
+                    Ok(bytes) => bytes,
+                    Err(err) => {
+                        return build_response(StatusCode::INTERNAL_SERVER_ERROR, err.to_string())
+                    }
+                };
+
+                match std::fs::write("/usr/simulink/GOcontroll_Linux.a2l", bytes) {
+                    Ok(_) => {
+                        _ = Command::new("systemctl")
+                            .arg("restart")
+                            .arg("nodered")
+                            .spawn();
+                        _ = Command::new("sync").spawn();
+                        return build_response(StatusCode::OK, "a2lFile uploaded!".to_owned());
+                    }
+                    Err(err) => {
+                        return build_response(
+                            StatusCode::INTERNAL_SERVER_ERROR,
+                            format!("Failed to write file to server.\n{}", err.to_string()),
+                        );
+                    }
+                }
+            }
+            "ovpnFile" => {
+                let Some(file_name) = field.file_name() else {
+                    return build_response(
+                        StatusCode::BAD_REQUEST,
+                        "No filename given, can't process\n".to_owned(),
+                    );
+                };
+                let Some(file_extension) = file_name.split(".").last() else {
+                    return build_response(
+                        StatusCode::BAD_REQUEST,
+                        "Could not get file extension, can't process\n".to_owned(),
+                    );
+                };
+                if file_extension != "ovpn" {
+                    return build_response(
+                        StatusCode::BAD_REQUEST,
+                        "Wrong file extension detected! Check your file!".to_owned(),
+                    );
+                }
+
+                if !std::path::Path::new("/etc/openvpn")
+                    .try_exists()
+                    .expect("can't check the existence of /etc/openvpn")
+                {
+                    std::fs::create_dir("/etc/openvpn").expect("can't create /etc/openvpn");
+                }
+
+                _ = std::fs::remove_file("/etc/openvpn/moduline.conf");
+
+                let bytes = match field.bytes().await {
+                    Ok(bytes) => bytes,
+                    Err(err) => {
+                        return build_response(StatusCode::INTERNAL_SERVER_ERROR, err.to_string())
+                    }
+                };
+
+                match std::fs::write("/etc/openvpn/moduline.conf", bytes) {
+                    Ok(_) => {
+                        if Command::new("systemctl")
                             .arg("is-active")
                             .arg("--quiet")
-                            .arg("go-simulink")
+                            .arg("openvpn")
                             .status()
                             .unwrap()
                             .success()
                         {
                             _ = Command::new("systemctl")
-                                .arg("stop")
-                                .arg("go-simulink")
-                                .status();
-                            true
-                        } else {
-                            false
-                        };
-
-                        _ = std::fs::remove_file("/usr/simulink/GOcontroll_Linux.elf");
-
-                        if !std::path::Path::new("/usr/simulink").try_exists().unwrap() {
-                            std::fs::create_dir("/usr/simulink").unwrap();
-                        }
-
-                        match field
-                            .data
-                            .save()
-                            .ignore_text()
-                            .with_path("/usr/simulink/GOcontroll_Linux.elf")
-                        {
-                            SaveResult::Full(_) => {
-                                _ = Command::new("chmod")
-                                    .args(["555", "/usr/simulink/GOcontroll_Linux.elf"])
-                                    .status();
-                                if restart {
-                                    _ = Command::new("systemctl")
-                                        .arg("start")
-                                        .arg("go-simulink")
-                                        .spawn();
-                                }
-                                _ = Command::new("sync").spawn();
-                                return Ok(build_response(
-                                    200,
-                                    "File uploaded! You can now close this tab/window.",
-                                ));
-                            }
-                            SaveResult::Error(err) => {
-                                return Ok(build_response(
-                                    400,
-                                    format!("Failed to write file to server.\n{}", err),
-                                ));
-                            }
-                            SaveResult::Partial(_res, err) => {
-                                return Ok(build_response(
-                                    400,
-                                    format!("Failed to write file to server.\n{:?}", err),
-                                ));
-                            }
+                                .arg("restart")
+                                .arg("openvpn")
+                                .spawn();
+                            _ = Command::new("sync").spawn();
+                            return build_response(StatusCode::OK, "ovpnFile uploaded!".to_owned());
                         }
                     }
-                    "a2lFile" => {
-                        let file_name = field.headers.filename.unwrap();
-                        let file_extension = file_name.split(".").last().unwrap();
-                        if file_extension != "a2l" {
-                            return Ok(build_response(
-                                400,
-                                "Wrong file extension detected! Check your file!",
-                            ));
-                        }
-
-                        _ = std::fs::remove_file("/usr/simulink/GOcontroll_Linux.a2l");
-
-                        match field
-                            .data
-                            .save()
-                            .with_path("/usr/simulink/GOcontroll_Linux.a2l")
-                        {
-                            SaveResult::Full(_) => {
-                                _ = Command::new("systemctl")
-                                    .arg("restart")
-                                    .arg("nodered")
-                                    .spawn();
-                                _ = Command::new("sync").spawn();
-                                return Ok(build_response(
-                                    200,
-                                    "File uploaded! You can now close this tab/window.",
-                                ));
-                            }
-                            SaveResult::Error(err) => {
-                                return Ok(build_response(
-                                    400,
-                                    format!("Failed to write file to server.\n{}", err),
-                                ));
-                            }
-                            SaveResult::Partial(_res, err) => {
-                                return Ok(build_response(
-                                    400,
-                                    format!("Failed to write file to server.\n{:?}", err),
-                                ));
-                            }
-                        }
-                    }
-                    "ovpnFile" => {
-                        let file_name = field.headers.filename.unwrap();
-                        let file_extension = file_name.split(".").last().unwrap();
-                        if file_extension != "ovpn" {
-                            return Ok(build_response(
-                                400,
-                                "Wrong file extension detected! Check your file!",
-                            ));
-                        }
-
-                        if !std::path::Path::new("/etc/openvpn").try_exists().unwrap() {
-                            std::fs::create_dir("/etc/openvpn").unwrap();
-                        }
-
-                        _ = std::fs::remove_file("/etc/openvpn/moduline.conf");
-
-                        match field.data.save().with_path("/etc/openvpn/moduline.conf") {
-                            SaveResult::Full(_) => {
-                                if Command::new("systemctl")
-                                    .arg("is-active")
-                                    .arg("--quiet")
-                                    .arg("openvpn")
-                                    .status()
-                                    .unwrap()
-                                    .success()
-                                {
-                                    _ = Command::new("systemctl")
-                                        .arg("restart")
-                                        .arg("openvpn")
-                                        .spawn();
-                                    _ = Command::new("sync").spawn();
-                                    return Ok(build_response(
-                                        200,
-                                        "File uploaded! You can now close this tab/window.",
-                                    ));
-                                }
-                            }
-                            SaveResult::Error(err) => {
-                                return Ok(build_response(
-                                    400,
-                                    format!("Failed to write file to server.\n{}", err),
-                                ));
-                            }
-                            SaveResult::Partial(_res, err) => {
-                                return Ok(build_response(
-                                    400,
-                                    format!("Failed to write file to server.\n{:?}", err),
-                                ));
-                            }
-                        }
-                    }
-                    _ => {
-                        return Ok(build_response(400, "Unknown file type received\n"));
+                    Err(err) => {
+                        return build_response(
+                            StatusCode::INTERNAL_SERVER_ERROR,
+                            format!("Failed to write file to server.\n{}", err.to_string()),
+                        );
                     }
                 }
             }
-            return Ok(build_response(
-                200,
-                "File uploaded! You can now close this tab/window.",
-            ));
+            _ => {
+                return build_response(
+                    StatusCode::BAD_REQUEST,
+                    "Could not process upload\n".to_owned(),
+                )
+            }
         }
-        Err(_) => Ok(build_response(400, "The request is not multipart")),
     }
+    build_response(
+        StatusCode::BAD_REQUEST,
+        "Could not process upload\n".to_owned(),
+    )
 }
 
-fn build_response<D: Into<Vec<u8>>>(status_code: u16, data: D) -> Response<RespBody> {
-    let data = data.into();
-    let data_len = data.len();
-    Response::new(
-        StatusCode(status_code),
-        vec![],
-        Cursor::new(data),
-        Some(data_len),
-        None,
-    )
+fn build_response(status_code: StatusCode, message: String) -> Response<String> {
+    Response::builder()
+        .status(status_code)
+        .body(message)
+        .unwrap()
 }
